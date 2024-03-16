77e55b7ecc0f8d53b2a44ca82242d600a44771b1
azaria-morake <azaria-morake@gmail.com>
2024-03-16 07:45:36 -0700

    Slightly  updated 'console.py' pycodestyle.

--- console.py
+++ console.py
@@ -2,25 +2,33 @@
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-	prompt = "(hbnb) "
-	
-	def do_quit(self, arg):
-		return True
-	
-	def do_EOF(self, arg):
-		return True
-	def help_quit(self):
-		print("Quit command to exit the program")
-	def do_create(self):
-	def do_show(self):
-	def do_destroy(self):
-	def do_all(self):
-	def do_update(self):
-	
-	def help_EOF(self):
-		print("Quit command to exit the program")
-	def emptyline(self):
-		pass
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+
+    def do_create(self):
+
+    def do_show(self):
+        self
+
+    def do_destroy(self):
+
+    def do_all(self):
+
+    def do_update(self):
+
+    def help_EOF(self):
+        print("Quit command to exit the program")
+    def emptyline(self):
+        pass
 if __name__ == '__main__':
 	HBNBCommand().cmdloop()
